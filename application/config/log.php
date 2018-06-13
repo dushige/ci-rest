5fd671e398b6f1d4fe9375267ee16a0ac183ffbe
kun.wang <kun.wang@beibei.com>
2018-06-13 09:23:21 +0800

    change log format

--- application/config/log.php
+++ application/config/log.php
@@ -2,6 +2,6 @@
 
 $config['log_path'] = '';
 $config['log_handlers'] = [\dkm\libraries\LogFactory::HANDLER_STREAM];
-$config['log_format'] = \dkm\libraries\LogFactory::FORMAT_JSON;
+$config['log_format'] = \dkm\libraries\LogFactory::FORMAT_LINE;
 $config['log_level'] = \dkm\libraries\LogFactory::LEVEL_INFO;
 $config['log_extension'] = '.log';
